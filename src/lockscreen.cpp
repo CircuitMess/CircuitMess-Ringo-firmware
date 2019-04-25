0d8b628fec54794197892ecb6eddc37a7e62350b
MiLeG <emil.gajsak@gmail.com>
2019-04-25 17:17:59 +0200

    Minor notification refactorings

--- src/lockscreen.cpp
+++ src/lockscreen.cpp
@@ -9,18 +9,18 @@ void drawNotificationWindow(uint8_t y, uint8_t index) {
 	mp.display.print(mp.notificationDescriprionList[index]);
 	switch (mp.notificationTypeList[index])
 	{
-		case NotificationType::Phone:
-			mp.display.drawBitmap(7, y + 2, missedcall, TFT_RED, 2);
+		case 1:
+			mp.display.drawBitmap(7, y + 2, missedCallIcon, TFT_RED, 2);
 			break;
-		case NotificationType::Messages:
-			mp.display.drawBitmap(7, y + 2, newtext, TFT_RED, 2);
+		case 2:
+			mp.display.drawBitmap(7, y + 2, newMessageIcon, TFT_BLACK, 2);
 			break;
-		case NotificationType::System:
+		case 3:
 			mp.display.drawBitmap(7, y + 2, systemNotification, TFT_RED, 2);
 			break;
 	}
 	mp.display.setTextFont(1);
-	mp.display.setCursor(120, y + 1);
+	mp.display.setCursor(120, y + 2);
 	String temp = "";
 	DateTime now = mp.notificationTimeList[index];
 	if (now.hour() < 10)
@@ -46,13 +46,6 @@ void lockscreen() {
     bool goOut = 0;
 	uint32_t elapsedMillis = millis();
     uint32_t buttonHeld;
-    uint8_t pixelState;
-    uint8_t scale;
-	if(mp.resolutionMode)
-		scale = 1;
-	else
-		scale = 2;
-
 	FastLED.clear();
 
 	while (1)
@@ -60,21 +53,21 @@ void lockscreen() {
 		mp.display.fillScreen(backgroundColors[mp.backgroundIndex]);
 
 		mp.display.setFreeFont(TT1);
-		mp.display.setTextSize(3*scale);//
+		mp.display.setTextSize(6);
 		//Hour shadow
 		mp.display.setTextColor(TFT_DARKGREY);
 		if (mp.clockHour == 11)
-			mp.display.setCursor(10*scale, 28*scale);
+			mp.display.setCursor(10*2, 28*2);
 		else if (mp.clockHour % 10 == 1 || (int)mp.clockHour / 10 == 1)
-			mp.display.setCursor(7*scale, 28*scale);
+			mp.display.setCursor(7*2, 28*2);
 		else
-			mp.display.setCursor(4*scale, 28*scale);
+			mp.display.setCursor(4*2, 28*2);
 		if (mp.clockHour < 10)
 			mp.display.print("0");
 		mp.display.print(mp.clockHour);
 
 		//minute shadow
-		mp.display.setCursor(36*scale, 28*scale);
+		mp.display.setCursor(36*2, 28*2);
 		if (mp.clockMinute < 10)
 			mp.display.print("0");
 		mp.display.print(mp.clockMinute);
@@ -82,23 +75,23 @@ void lockscreen() {
 		//Hour black
 		mp.display.setTextColor(TFT_BLACK);
 		if (mp.clockHour == 11)
-			mp.display.setCursor(9*scale, 27*scale);
+			mp.display.setCursor(9*2, 27*2);
 		else if (mp.clockHour % 10 == 1 || (int)mp.clockHour / 10 == 1)
-			mp.display.setCursor(6*scale, 27*scale);
+			mp.display.setCursor(6*2, 27*2);
 		else
-			mp.display.setCursor(3*scale, 27*scale);
+			mp.display.setCursor(3*2, 27*2);
 		if (mp.clockHour < 10)
 			mp.display.print("0");
 		mp.display.print(mp.clockHour);
 
 		//Minute black
-		mp.display.setCursor(35*scale, 27*scale);
+		mp.display.setCursor(35*2, 27*2);
 		if (mp.clockMinute < 10)
 			mp.display.print("0");
 		mp.display.print(mp.clockMinute);
 
-		mp.display.setTextSize(1*scale);
-		mp.display.setCursor(60*scale, 19*scale);
+		mp.display.setTextSize(1*2);
+		mp.display.setCursor(60*2, 19*2);
 		mp.display.setTextWrap(false);
 		if (mp.clockDay < 10)
 			mp.display.print("0");
@@ -107,7 +100,7 @@ void lockscreen() {
 		if (mp.clockMonth < 10)
 			mp.display.print("0");
 		mp.display.print(mp.clockMonth);
-		mp.display.setCursor(62*scale, 25*scale);
+		mp.display.setCursor(62*2, 25*2);
 		mp.display.print(2000 + mp.clockYear);
 		/*mp.display.setTextSize(2);
 		mp.display.setCursor(10, 50);
@@ -116,21 +109,21 @@ void lockscreen() {
 		if (mp.simInserted && !mp.airplaneMode)
 		{
 			if (mp.signalStrength <= 3)
-				mp.display.drawBitmap(1*scale, 1*scale, noSignalIcon, TFT_BLACK, scale);
+				mp.display.drawBitmap(2, 2, noSignalIcon, TFT_BLACK, 2);
 			else if (mp.signalStrength > 3 && mp.signalStrength <= 10)
-				mp.display.drawBitmap(1*scale, 1*scale, signalLowIcon, TFT_BLACK, scale);
+				mp.display.drawBitmap(2, 2, signalLowIcon, TFT_BLACK, 2);
 			else if (mp.signalStrength > 10 && mp.signalStrength <= 20)
-				mp.display.drawBitmap(1*scale, 1*scale, signalHighIcon, TFT_BLACK, scale);
+				mp.display.drawBitmap(2, 2, signalHighIcon, TFT_BLACK, 2);
 			else if (mp.signalStrength > 20 && mp.signalStrength <= 31)
-				mp.display.drawBitmap(1*scale, 1*scale, signalFullIcon, TFT_BLACK, scale);
+				mp.display.drawBitmap(2, 2, signalFullIcon, TFT_BLACK, 2);
 			else if (mp.signalStrength == 99)
-				mp.display.drawBitmap(1*scale, 1*scale, signalErrorIcon, TFT_BLACK, scale);
+				mp.display.drawBitmap(2, 2, signalErrorIcon, TFT_BLACK, 2);
 		}
 		else if(!mp.simInserted && !mp.airplaneMode)
-			mp.display.drawBitmap(1*scale, 1*scale, signalErrorIcon, TFT_BLACK, scale);
+			mp.display.drawBitmap(2, 2, signalErrorIcon, TFT_BLACK, 2);
 		if (mp.volume == 0)
 		{
-			mp.display.drawBitmap(helper*scale, 1*scale, silentmode, TFT_BLACK, scale);
+			mp.display.drawBitmap(helper*2, 2, silentModeIcon, TFT_BLACK, 2);
 			helper += 10;
 		}
 		//mp.display.drawBitmap(31, 1, missedcall);
@@ -138,103 +131,84 @@ void lockscreen() {
 		if (!mp.airplaneMode)
 		{
 			if (mp.wifi == 1)
-				mp.display.drawBitmap(helper*scale, 1*scale, wifion, TFT_BLACK, scale);
+				mp.display.drawBitmap(helper * 2, 2, wifiOnIcon, TFT_BLACK, 2);
 			else
-				mp.display.drawBitmap(helper*scale, 1*scale, wifioff, TFT_BLACK, scale);
+				mp.display.drawBitmap(helper * 2, 2, wifiOffIcon, TFT_BLACK, 2);
 			helper += 10;
 			if (mp.bt)
-				mp.display.drawBitmap(helper*scale, 1*scale, BTon, TFT_BLACK, scale);
+				mp.display.drawBitmap(helper * 2, 2, BTonIcon, TFT_BLACK, 2);
 			else
-				mp.display.drawBitmap(helper*scale, 1*scale, BToff, TFT_BLACK, scale);
+				mp.display.drawBitmap(helper * 2, 2, BToffIcon, TFT_BLACK, 2);
 			helper += 10;
 		}
 		else
 		{
-			mp.display.drawBitmap(scale, scale, airplaneModeIcon, TFT_BLACK, scale);
+			mp.display.drawBitmap(helper * 2, 2, airplaneModeIcon, TFT_BLACK, 2);
 			helper += 10;
 		}
 
 		if(!mp.SDinsertedFlag)
-			mp.display.drawBitmap(helper*scale, 1*scale, noSDIcon, TFT_BLACK, scale);
+			mp.display.drawBitmap(helper * 2, 2, noSDIcon, TFT_BLACK, 2);
 
 		if (mp.batteryVoltage > 4000)
-			mp.display.drawBitmap(74*scale, 1*scale, batteryCharging, TFT_BLACK, scale);
+			mp.display.drawBitmap(148, 2, batteryChargingIcon, TFT_BLACK, 2);
 		else if (mp.batteryVoltage <= 4000 && mp.batteryVoltage >= 3800)
-			mp.display.drawBitmap(74*scale, 1*scale, batteryFull, TFT_BLACK, scale);
+			mp.display.drawBitmap(148, 2, batteryFullIcon, TFT_BLACK, 2);
 		else if (mp.batteryVoltage < 3800 && mp.batteryVoltage >= 3700)
-			mp.display.drawBitmap(74*scale, 1*scale, batteryMid, TFT_BLACK, scale);
+			mp.display.drawBitmap(148, 2, batteryHighIcon, TFT_BLACK, 2);
 		else if (mp.batteryVoltage < 3700 && mp.batteryVoltage >= 3600)
-			mp.display.drawBitmap(74*scale, 1*scale, batteryMidLow, TFT_BLACK, scale);
+			mp.display.drawBitmap(148, 2, batteryMidIcon, TFT_BLACK, 2);
 		else if (mp.batteryVoltage < 3600 && mp.batteryVoltage >= 3500)
-			mp.display.drawBitmap(74*scale, 1*scale, batteryLow, TFT_BLACK, scale);
+			mp.display.drawBitmap(148, 2, batteryLowIcon, TFT_BLACK, 2);
 		else if (mp.batteryVoltage < 3500)
-			mp.display.drawBitmap(74*scale, 1*scale, batteryEmpty, TFT_BLACK, scale);
-		bool anyNotifications = 0;
+			mp.display.drawBitmap(148, 2, batteryEmptyIcon, TFT_BLACK, 2);
+
 		uint8_t temp = 0;
 		for(int i = 0; i< sizeof(mp.notificationTypeList);i++)
 		{
-			if(mp.notificationTypeList[i] != NotificationType::NONE)
+			if(mp.notificationTypeList[i] != 0)
 			{
 				temp++;
 				if(temp == 1)
 					drawNotificationWindow(64,i);
 				else if(temp == 2)
 					drawNotificationWindow(88,i);
-				anyNotifications = 1;
 			}
 		}
 
-		if(!anyNotifications)
-		{
-			           
-		}
-		// drawNotificationWindow(64,"Missed call from Dad");
-		// drawNotificationWindow(88, "Text from Jack");
 		mp.display.setFreeFont(TT1);
-		mp.display.setTextSize(2*scale);
+		mp.display.setTextSize(4);
 		if (millis() - elapsedMillis >= 500) {
 			elapsedMillis = millis();
 			blinkState = !blinkState;
 			if (mp.clockYear != 4 && mp.clockYear != 80)
 				mp.updateTimeRTC();
 		}
-
-
 		if (blinkState == 1)
 		{
+			mp.display.setCursor(2, 111);
+			mp.display.setTextFont(2);
 			mp.display.setTextSize(1);
-			if(scale == 1)
-			{
-				mp.display.setCursor(1, 63);
-				mp.display.setFreeFont(TT1);
-			}
-			else
-			{
-				/* mp.display.setCursor(2, 63*scale);
-				mp.display.setFreeFont(FSS9); */
-				mp.display.setCursor(2, 111);
-				mp.display.setTextFont(2);
-			}
 			mp.display.setTextColor(TFT_BLACK);
 			mp.display.print("Hold \"A\" to unlock");
-			mp.display.setTextSize(3*scale);
+			mp.display.setTextSize(6);
 			mp.display.setTextColor(TFT_DARKGREY);
 			mp.display.setFreeFont(TT1);
-			mp.display.setCursor(29*scale, 28*scale);
+			mp.display.setCursor(58, 56);
 			mp.display.print(":");
 			mp.display.setTextColor(TFT_BLACK);
-			mp.display.setCursor(28*scale, 27*scale);
+			mp.display.setCursor(56, 54);
 			mp.display.print(":");
 		}
 		mp.update();
 
-		mp.display.setTextSize(1*scale);
+		mp.display.setTextSize(2);
 
 		if (mp.buttons.pressed(BTN_A)) {
 			mp.display.setTextSize(1);
 			// vibration(200);
 
-			mp.display.fillRect(0, 56*scale, mp.display.width(), 7*scale, backgroundColors[mp.backgroundIndex]);
+			mp.display.fillRect(0, 112, mp.display.width(), 14, backgroundColors[mp.backgroundIndex]);
 			if(mp.resolutionMode)
 			{
 				mp.display.setCursor(1, 63);
@@ -248,12 +222,11 @@ void lockscreen() {
 			mp.display.print("Unlocking");
 			mp.update();
 			buttonHeld = millis();
-			pixelState = 0;
 			while (mp.buttons.kpd.pin_read(BTN_A) == 0)
 			{
 
 				if (millis() - buttonHeld > 250 && millis() - buttonHeld < 500) {
-					mp.display.fillRect(0, 57*scale, mp.display.width(), 7*scale, backgroundColors[mp.backgroundIndex]);
+					mp.display.fillRect(0, 114, mp.display.width(), 14, backgroundColors[mp.backgroundIndex]);
 					if(mp.resolutionMode)
 					{
 						mp.display.setCursor(1, 63);
@@ -270,7 +243,7 @@ void lockscreen() {
 				}
 				else if (millis() - buttonHeld > 500 && millis() - buttonHeld < 750)
 				{
-					mp.display.fillRect(0, 57*scale, mp.display.width(), 7*scale, backgroundColors[mp.backgroundIndex]);
+					mp.display.fillRect(0, 114, mp.display.width(), 14, backgroundColors[mp.backgroundIndex]);
 					if(mp.resolutionMode)
 					{
 						mp.display.setCursor(1, 63);
@@ -289,7 +262,7 @@ void lockscreen() {
 				}
 				else if (millis() - buttonHeld > 750 && millis() - buttonHeld < 1000)
 				{
-					mp.display.fillRect(0, 57*scale, mp.display.width(), 7*scale, backgroundColors[mp.backgroundIndex]);
+					mp.display.fillRect(0, 114, mp.display.width(), 14, backgroundColors[mp.backgroundIndex]);
 					if(mp.resolutionMode)
 					{
 						mp.display.setCursor(1, 63);
