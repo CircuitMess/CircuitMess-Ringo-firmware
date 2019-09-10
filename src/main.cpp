b185ac367dfc2c3fe7a5d60be6a1a4ce2153a978
robbie8-bit <53393837+robbie8-bit@users.noreply.github.com>
2019-09-10 10:19:06 +0200

    Snake update

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "MAKERphone.h"
 #include <FastLED/FastLED.h>
 #include <utility/Buttons/Buttons.h>
+#include <utility/soundLib/MPWavLib.h>
 
 #define EYE_COLOR    0x0000       /* TFT_GREEN */
 
@@ -10,14 +11,16 @@ MAKERphone mp;
 boolean vFlag;
 boolean hFlag;
 boolean eaten;
-boolean borderFlag;
+boolean borderFlag = 1;
 boolean pressed;
 boolean snakeMenuInt;
 boolean firstTime;
 
-uint8_t speed = 2;
+Oscillator *osc;
+
+uint8_t speed = 1;
 const int maxSnakeLength = 600;
-uint8_t snakeLength = 8;
+uint16_t snakeLength = 12;
 uint8_t hScore;
 
 int snakeX[maxSnakeLength];
@@ -54,15 +57,15 @@ void drawFood(){
 }
 void drawSnake()
 {
-  for (int i = (snakeLength/speed) - 1; i >= 0; i--)
+  for (int i = (snakeLength) - 1; i >= 0; i--)
   {
-/* 	if ( i > snakeLength - 5) snakeColor = 0;	  
-	else if(i < 4 || (i <= snakeLength-6 &&  i % 2 == 1)) snakeColor = 1;
-	else snakeColor = 0; */
+ 	if ( i > (snakeLength) - 5) snakeColor = 0;	  
+	else if(i < 4 || (i <= (snakeLength) - 6 &&  i % 2 == 1)) snakeColor = 1;
+	else snakeColor = 0; 
 	if (foodX == snakeX[i] && foodY == snakeY[i]) drawFood();
     //else snakeColor = 0;
-	//if(snakeColor == 0) mp.display.fillRect(snakeX[i], snakeY[i], tileSize, tileSize,TFT_DARKGREEN);
-    mp.display.fillRect(snakeX[i], snakeY[i], tileSize, tileSize,TFT_GREEN);
+	if(snakeColor == 0) mp.display.fillRect(snakeX[i], snakeY[i], tileSize, tileSize,TFT_DARKGREEN);
+    else mp.display.fillRect(snakeX[i], snakeY[i], tileSize, tileSize,TFT_GREEN);
   }
   if(dirX == 1*speed)  {
 	mp.display.drawRect(snakeX[0]+3, snakeY[0]+1, 2, 3,TFT_RED);
@@ -143,14 +146,17 @@ void foodCheck(){
   else if ((snakeX[0] == foodX || snakeX[0] == foodX+3) && (snakeY[0]+2 == foodY || snakeY[0]+2 == foodY+3 )) eaten = true;  
   if(eaten){
     mp.display.fillRect(foodX,foodY, foodSize, foodSize, TFT_BLACK);
-    snakeLength+=6;
-    hScore+=(1*(speed-1));
+    snakeLength+=6/speed;
+    hScore+=(1*speed);
+	osc->setWaveform(SQUARE);
+	osc->note(90,0.03);
+	FastLED.showColor(CRGB::Yellow);
     drawFood();
   }
 }
 void control()
 {
-  for(int i = snakeLength/speed; i  > 0; i--)
+  for(int i = snakeLength; i  > 0; i--)
   {
     snakeX[i] = snakeX[i-1];
     snakeY[i] = snakeY[i-1];
@@ -169,7 +175,7 @@ void reset(){
     dirY = 0;
     hFlag = true;
     vFlag = false;
-    snakeLength = 8;
+    snakeLength = 12/speed;
     hScore = 0;
   //  mp.display.drawRect(2,1,156,126,TFT_WHITE);
     while(!mp.update());
@@ -178,14 +184,26 @@ void reset(){
 }
 void touchedItSelf()
 {
-  for (int i = 1; i < snakeLength/speed; i++)
+  for (int i = 1; i < snakeLength; i++)
   {
     // if ((snakeX[0] == snakeX[i]) && (snakeY[0] == snakeY[i]))
     if (
     ((snakeX[0] == snakeX[i+10] || snakeX[0] == snakeX[i+10]+4) && (snakeY[0] == snakeY[i+10] || snakeY[0] == snakeY[i+10]+4))
     || ((snakeX[0]+4 == snakeX[i+10] || snakeX[0]+4 == snakeX[i+10]+4) && (snakeY[0]+4 == snakeY[i+10] || snakeY[0]+4 == snakeY[i+10]+4)))
     {
-      delay(1000);
+		FastLED.showColor(CRGB::DarkRed);
+		osc->setWaveform(SAW);
+		osc->note(40,0.4);
+    	delay(1000);
+	  	mp.display.fillScreen(TFT_BLACK);
+	    mp.display.setCursor(10, 40, 2);
+        mp.display.setTextSize(2);
+        mp.display.setTextColor(TFT_YELLOW);
+        mp.display.print(F("Your score"));
+        mp.display.setCursor(65, 70);
+        mp.display.print(hScore);
+        while(!mp.update());
+        delay(2000);
       reset();
     }
   }
@@ -193,12 +211,16 @@ void touchedItSelf()
 void crash(){
     if(borderFlag) {
       if ((snakeX[0] <= 0 || snakeY[0] <= 0 || snakeX[0] >= 155 || snakeY[0] >= 124)){
+		FastLED.showColor(CRGB::DarkRed);  
+		osc->setWaveform(SAW);
+		osc->note(40,0.4);
         delay(1000);
-        mp.display.setCursor(20, 40, 2);
+		mp.display.fillScreen(TFT_BLACK);
+        mp.display.setCursor(10, 40, 2);
         mp.display.setTextSize(2);
         mp.display.setTextColor(TFT_YELLOW);
         mp.display.print(F("Your score"));
-        mp.display.setCursor(45, 70);
+        mp.display.setCursor(65, 70);
         mp.display.print(hScore);
         while(!mp.update());
         delay(2000);
@@ -206,7 +228,7 @@ void crash(){
       }
     }
     else {
-      for(int i = 0; i < snakeLength/speed; i++){
+      for(int i = 0; i < snakeLength; i++){
       if(snakeX[i] < 0 && snakeX[i] > -5) snakeX[i] = 159;
       else if (snakeX[i] < -5);
       else snakeX[i] = snakeX[i]%159;
@@ -216,12 +238,6 @@ void crash(){
 
       }
     }
-}
-void menuSnake(){
-  snakeLength = 30;
-
-  dirX = 1*speed;
-  dirY = 0;
 }
 void buttonDir(){
 //	mp.buttons.update();
@@ -257,64 +273,91 @@ void buttonDir(){
     pressed = false;
    // mp.buttons.update();
 }
-void mm(){
+void startAnimation(){
   mp.display.fillScreen(TFT_BLACK);
-  mp.display.setCursor(30, 30, 1);
+  mp.display.setCursor(20, 50, 1);
   mp.display.setTextColor(TFT_GREEN);
-  mp.display.setTextSize(3);
+  mp.display.setTextSize(4);
   mp.display.println(F("SNAKE"));
   while(!mp.update());
-  delay(1000);
+  FastLED.showColor(CRGB::Green);
+  delay(1500);
+}
+void mm(){
+
   defaultInt();
+  osc->setWaveform(SINE);
   while(!mp.buttons.pressed(BTN_A)){
     mp.display.fillScreen(TFT_BLACK);
-    mp.display.setCursor(50, 8, 1);
+	snakeMenu();
+    control();
+    drawSnake();
+    mp.display.setCursor(50, 9, 1);
     mp.display.setTextColor(TFT_GREEN);
     mp.display.setTextSize(2);
     mp.display.println(F("SNAKE"));
-    snakeMenu();
-    control();
-    drawSnake();
+ 
     if(mp.buttons.pressed(BTN_DOWN)){
-      menuSignal++;
-      menuSignal%=4;
+		//osc->setVolume(80);
+  		osc->note(60,0.05);
+    	menuSignal++;
+    	menuSignal%=4;
     }
     if(mp.buttons.pressed(BTN_UP)){
-      if(menuSignal !=0)menuSignal--;
-      else menuSignal = 3;
+		osc->note(60,0.05);
+	   	if(menuSignal !=0)menuSignal--;
+    	else menuSignal = 3;
     }
-    if(mp.buttons.pressed(BTN_RIGHT) && menuSignal==3){
-      speed = 3;
+    if(mp.buttons.pressed(BTN_RIGHT) && menuSignal == 1 && speed == 1){
+		osc->note(50,0.05);
+		FastLED.showColor(CRGB::DarkGreen);
+    	speed = 2;
     }
-    if(mp.buttons.pressed(BTN_LEFT) && menuSignal ==3){
-      speed = 2;
+    if(mp.buttons.pressed(BTN_LEFT) && menuSignal == 1 && speed == 2){
+		osc->note(50,0.05);
+		FastLED.showColor(CRGB::LimeGreen);
+        speed = 1;
+    }
+	if(mp.buttons.pressed(BTN_RIGHT) && menuSignal == 0 && borderFlag == 1){
+		osc->note(50,0.05);
+		FastLED.showColor(CRGB::DarkGreen);
+    	borderFlag = 0;
+    }
+    if(mp.buttons.pressed(BTN_LEFT) && menuSignal == 0 && borderFlag == 0){
+		osc->note(50,0.05);
+		FastLED.showColor(CRGB::LimeGreen);
+        borderFlag = 1;
     }
     mp.display.setTextSize(2);
     //mp.display.fillScreen(TFT_BLACK);
     mp.display.setTextColor(TFT_DARKGREY);
-    if(menuSignal == 0)  mp.display.setTextColor(TFT_YELLOW);
-    mp.display.setCursor(30, 18, 2);
-    mp.display.print(F("BORDER"));
+    if(borderFlag == 1 && menuSignal == 0)  mp.display.setTextColor(TFT_YELLOW);
+    mp.display.setCursor(9, 22, 2);
+    mp.display.print(F("WALL"));
+    mp.display.setTextColor(TFT_DARKGREY);
+	mp.display.print(F("/"));
+    if(menuSignal == 0 && borderFlag == 0)mp.display.setTextColor(TFT_YELLOW);
+    mp.display.setCursor(88, 22, 2);
+    mp.display.print(F("FREE"));
     mp.display.setTextColor(TFT_DARKGREY);
     if(menuSignal == 1)mp.display.setTextColor(TFT_YELLOW);
-    mp.display.setCursor(12, 42, 2);
-    mp.display.print(F("NO BORDER"));
+    mp.display.setCursor(25, 46, 2);
+	mp.display.print("SPEED: ");
+    mp.display.print(speed);
     mp.display.setTextColor(TFT_DARKGREY);
     if(menuSignal == 2)mp.display.setTextColor(TFT_YELLOW);
-    mp.display.setCursor(54, 68, 2);
+    mp.display.setCursor(29, 70, 2);
+    mp.display.print(F("SCORES"));
+	mp.display.setTextColor(TFT_DARKGREY);
+	if(menuSignal == 3)mp.display.setTextColor(TFT_YELLOW);
+    mp.display.setCursor(54, 94, 2);
     mp.display.print(F("EXIT"));
-    mp.display.setTextColor(TFT_DARKGREY);
-    if(menuSignal == 3)mp.display.setTextColor(TFT_YELLOW);
-    mp.display.setCursor(25, 92, 2);
-    mp.display.print("SPEED: ");
-    mp.display.print(speed-1);
 
-    while(!mp.update());
+    mp.update(0);
   }
   snakeMenuInt = 0;
-  if(menuSignal == 0) borderFlag = 1;
-  if(menuSignal == 1) borderFlag = 0;
-  if(menuSignal == 2) mp.loader();
+  //if(menuSignal == 2);
+  if(menuSignal == 3) mp.loader();
 
 
   delay(1000);
@@ -323,15 +366,19 @@ void mm(){
 void setup() {
   Serial.begin(115200);
   mp.begin();
+  osc = new Oscillator(SINE);
+  addOscillator(osc);
+  osc->setVolume(60);
+  startAnimation();
   mm();
   reset();
 }
 void loop() {
-  while(!mp.update());
- // mp.update();
+  //while(!mp.update());
+  mp.update(0);
   //----------------------------------
   millisTwo = millis();
-  if(millisTwo - millisOne > 100) buttonDir();
+  if(millisTwo - millisOne > 80) buttonDir();
   // ---------------------------------
   mp.display.fillScreen(TFT_BLACK);
   if(borderFlag) mp.display.drawRect(0,0,160,128,TFT_WHITE);
@@ -343,10 +390,10 @@ void loop() {
   // ---------------------------------
   foodCheck();
   control();
-  crash();
   drawSnake();
+  crash();
   touchedItSelf();
-	//----------------------------------
+  //----------------------------------
   mp.display.fillRect(foodX, foodY, foodSize, foodSize, TFT_YELLOW);
 
 }
